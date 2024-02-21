68a350e01bf56e6ef6f11f7027e1898ae36deaa4
hk-kusumi <91217989+hk-kusumi@users.noreply.github.com>
2024-02-22 00:29:52 +0530

    first commit

---
+++ EfCore/EF_DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApplication.EfContext;
+using System.Data;
+
+namespace ShoppingApplication.EfCore
+{
+    public class EF_DataContext : DbContext
+    {
+        public EF_DataContext(DbContextOptions<EF_DataContext> options) : base (options) { }
+        public DbSet<Product> Products {  get; set; }
+        public DbSet<Order> Orders {  get; set; }
+    }
+}
